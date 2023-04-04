f5e2f8a89b60413671537159f52c9aba59be8a8a
Cyber <cybr1998@gmail.com>
2023-04-04 21:17:21 +0530

    fixing a bug

--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,31 @@ fn main() {
     let mut rng  = rand::thread_rng();
     let empty_indices: Vec<usize> = indices.choose_multiple(&mut rng, empty_position_count).cloned().collect();
 
-    let mut remaining_attempts = 3;
-    println!("Try to guess this word: {}" , word);
-    println!("Type the first character and then move on to the next!");
+    for idx in empty_indices.iter() {
+        word_chars[*idx] = '_';
+    }
 
-    for idx in empty_indices {
+    let mut remaining_attempts = 3;
+    let mut word_with_missing_chars: String = word_chars.iter().collect();
+    println!("Try to guess this word: {}" , word_with_missing_chars);
+
+    for idx in empty_indices.iter() {
         if remaining_attempts == 0 {
             println!("You lost!. Try again.");
             break
         }
-        println!("Enter the character. This character has {} attempts remaining", remaining_attempts);
+        println!("Enter the {}th character. This character has {} attempts remaining", *idx, remaining_attempts);
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).expect("Failed to read input");
         let ch: char = input.trim().to_lowercase().chars().nth(0).expect("Failed to convert to character");
-        if ch != word_chars[idx] {
+
+        if ch != word_chars[*idx] {
             remaining_attempts -= 1;
         }
         else {
-            continue
+            word_chars[*idx] = ch;
+            word_with_missing_chars = word_chars.iter().collect();
+            println!("Congratulations! You got this letter correct. The word now is {}", word_with_missing_chars);
         }
             
     }
