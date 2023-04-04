966c9974829995651744db9337253aecc879218f
Cyber <cybr1998@gmail.com>
2023-04-04 19:34:55 +0530

    boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    let word: &str = "cyber";
+    let mut word_chars: Vec<char> = word.chars().collect();
+    let guess = 'y';
+    let guess_position = 1;
+    if word_chars[guess_position] == guess {
+        println!("Yay, correct guess!");
+    }
+    else {
+        println!("Wrong guess. {} more attempts left.", 1);
+    }
 }
